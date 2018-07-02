d2c02391bb926a0d7c636454f37f80dfe9567d27
David Megginson <david.megginson@megginson.com>
2018-07-01 23:13:50 -0400

    Add unit for Mali activities.

--- demo/demo.js
+++ demo/demo.js
@@ -10,7 +10,8 @@ var mapConfig = {
         {
             name: "Mali 3W",
             url: "https://data.humdata.org/dataset/d7ab89e4-bcb2-4127-be3c-5e8cf804ffd3/resource/b8f708da-e596-456c-b550-f88959970d21/download/mali_3wop_decembre-2017.xls",
-            country: "MLI"
+            country: "MLI",
+            unit: "activities"
         }
     ]
 }
