951b30992f4698b1b690d1b4e0fcce9d48ec6729
David Megginson <david.megginson@megginson.com>
2018-07-18 20:46:14 -0400

    Tweaks to setup page.

--- demo/setup.js
+++ demo/setup.js
@@ -2,10 +2,13 @@ window.onload = function () {
     var form = document.forms[0];
     form.onsubmit = function () {
         var jsonField = form.elements["config"];
+        var titleNode = document.getElementById('map-title');
         var linkNode = document.getElementById('map-link');
-        console.log(linkNode);
         try {
             var json = JSON.parse(jsonField.value);
+            if (json.title) {
+                titleNode.textContent = "[Preview] " + json.title;
+            }
             var mapUrl = "index.html?config=" + encodeURIComponent(JSON.stringify(json));
             linkNode.setAttribute("href", mapUrl);
             linkNode.textContent = linkNode.href;
